bad46c930c6b6d2e7d2a5c023a92a2d7f1d48372
rmkim7 <rmjkim7@gmail.com>
2025-03-30 18:22:15 +0900

    feat: PostDetail을 서버 컴포넌트로 변경 및 데이터 패칭 방식 변경

--- apps/web/features/post/post-detail/ui/post-detail.tsx
+++ apps/web/features/post/post-detail/ui/post-detail.tsx
@@ -1,45 +1,39 @@
-'use client';
-
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-
 import { getPostById, mapPostToViewModel } from '@/entities/post';
 import { PostViewModel } from '@/entities/post/model/post-view-model';
 
-export function PostDetail() {
-  const { id } = useParams();
-  const [post, setPost] = useState<PostViewModel | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+interface PostDetailProps {
+  params: { id: string };
+}
 
-  useEffect(() => {
-    if (!id) return;
+export async function PostDetail({ params }: PostDetailProps) {
+  let post: PostViewModel | null = null;
+  let error: string | null = null;
 
-    async function fetchPost() {
-      try {
-        setIsLoading(true);
-        const data = await getPostById(id as string);
-        setPost(mapPostToViewModel(data));
-      } catch (err) {
-        setError('게시글을 불러오는 중 오류가 발생했습니다.');
-        console.error(err);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchPost();
-  }, [id]);
+  const id = (await params).id;
 
-  if (isLoading) return <p>로딩 중...</p>;
+  try {
+    const data = await getPostById(id);
+    post = mapPostToViewModel(data);
+  } catch (err) {
+    console.error(err);
+    error = '게시글을 불러오는 중 오류가 발생했습니다.';
+  }
+
   if (error) return <p>{error}</p>;
   if (!post) return <p>게시글을 찾을 수 없습니다.</p>;
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-      <p>작성자: {post.author}</p>
-      <p>작성일: {post.localeCreatedAt}</p>
+    <div key={id} className="mx-8 my-4 rounded border border-gray-400 p-4">
+      <h3 className="pb-4 font-semibold">{post.title}</h3>
+      <hr />
+      <p className="pt-4">{post.content}</p>
+      <br />
+      <div className="flex flex-col gap-2">
+        <span className="rounded border border-gray-300 bg-gray-300 px-2 py-0.5 italic">
+          {post.author}
+        </span>
+        <time>{post.localeCreatedAt}</time>
+      </div>
     </div>
   );
 }
